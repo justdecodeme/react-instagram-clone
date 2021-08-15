68267f29199c75c9917eec10d8c85388a3461b3e
Rakesh <justdecodeme@hotmail.com>
2021-08-15 22:32:15 +0530

    new components added

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import Header from "../components/header";
+import Sidebar from "../components/sidebar";
+import Timeline from "../components/timeline";
 
 const Dashboard = () => {
-	return <div>Dashboard</div>;
+	useEffect(() => {
+		document.title = "Instagram";
+	}, []);
+
+	return (
+		<div>
+			<Header />
+			<div>
+				<Timeline />
+				<Sidebar />
+			</div>
+		</div>
+	);
 };
 
 export default Dashboard;
