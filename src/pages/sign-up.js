2c1545d7ef60c5c72353bf990f4f9ca7038f853e
Rakesh <justdecodeme@hotmail.com>
2021-08-15 21:04:29 +0530

    signup screen added

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -4,23 +4,25 @@ import { useHistory, Link } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
 import * as ROUTES from "../constants/routes";
 
-const Login = () => {
+const SignUp = () => {
 	const history = useHistory();
 	const { firebase } = useContext(FirebaseContext);
 
+	const [username, setUsername] = useState("");
+	const [fullName, setFullName] = useState("");
 	const [emailAddress, setEmailAddress] = useState("");
 	const [password, setPassword] = useState("");
 
 	const [error, setError] = useState("");
 	const isInvalid = password === "" || emailAddress === "";
 
-	const handleLogin = async (e) => {
+	const handleSignUp = async (e) => {
 		e.preventDefault();
 		setError("");
 
 		try {
 			await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
-			console.log("login success...");
+			console.log("SignUp success...");
 			// history.push(ROUTES.DASHBOARD);
 		} catch (error) {
 			setError(error.message);
@@ -28,16 +30,30 @@ const Login = () => {
 	};
 
 	useEffect(() => {
-		document.title = "Login - Instagram";
+		document.title = "SignUp - Instagram";
 	}, []);
 
 	return (
 		<div style={{ maxWidth: "200px", margin: "0 auto", padding: "20px" }}>
 			{error && <p style={{ color: "red" }}>{error}</p>}
 
-			<form onSubmit={handleLogin} method="POST">
+			<form onSubmit={handleSignUp} method="POST">
 				<fieldset>
-					<legend>Log In</legend>
+					<legend>Sign Up</legend>
+					<input
+						type="text"
+						placeholder="Enter your username"
+						onChange={({ target }) => setUsername(target.value)}
+						value={username}
+					/>
+					<br />
+					<input
+						type="text"
+						placeholder="Full Name"
+						onChange={({ target }) => setFullName(target.value)}
+						value={fullName}
+					/>
+					<br />
 					<input
 						type="text"
 						placeholder="Email address"
@@ -52,13 +68,13 @@ const Login = () => {
 						value={password}
 					/>
 					<br />
-					<button disabled={isInvalid}>Log In</button>
+					<button disabled={isInvalid}>Sign up</button>
 				</fieldset>
 			</form>
 
-			<Link to={ROUTES.SIGN_UP}>Signup</Link>
+			<Link to={ROUTES.LOGIN}>Login</Link>
 		</div>
 	);
 };
 
-export default Login;
+export default SignUp;
